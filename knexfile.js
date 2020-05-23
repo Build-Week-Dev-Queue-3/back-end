b56e9cfac8bd585383b003d33dfa4e3c5d7e66ce
Marc Tapp <marc.tapp@gmail.com>
2020-05-23 10:51:41 -0400

    set up postgres

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,6 @@
-// Update with your config settings.
+// bring in the .env file
+require('dotenv').config();
+const connectionString = process.env.DATABASE_URL;
 
 module.exports = {
 
@@ -39,18 +41,22 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    connection: connectionString || {
+      host: "localhost",
+      database: 'devdeskbw',
+      user:     'postgres',
+      password: 'marctapp'
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './data/migrations',
+    },
+    seeds: { 
+      directory: './data/seeds', 
+    },
   }
 
 }
